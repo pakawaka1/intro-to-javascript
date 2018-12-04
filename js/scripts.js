e4f11302d94cffd99f00c2b38c4be2fefa437224
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2018-12-03 23:44:07 -0800

    time intervals

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 // Timers and set intervals
-const newPrompt = prompt("How much time would you like to wait (in milliseconds)?");
-console.log(newPrompt);
+// const newPrompt = prompt("How much time would you like to wait (in milliseconds)?");
+// console.log(newPrompt);
 
 
-function alert(newPrompt) {
+    const newPrompt = prompt("How much time would you like to wait (in milliseconds)?");
+
+    console.log(newPrompt);
     // const prompt = prompt("How much time would you like to wait (in milliseconds)?");
     const timer = setTimeout(quote, newPrompt);
     console.log(timer);
-};
 
 function quote() {
     alert('Hello World......')
