097d0854a9b1406c5cf57168fcaab83941210bf5
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2018-12-04 07:20:23 -0800

    setTimeout and setInterval

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
-// // // Timers and set intervals
+// // // Set and Clear Timeout 
 
-const newPrompt = prompt("How much time would you like to wait (in milliseconds)?");
-console.log(newPrompt);
-const timer = setTimeout(quote, newPrompt);
+// const newPrompt = prompt("How much time would you like to wait (in milliseconds)?");
+// console.log(newPrompt);
+// const timer = setTimeout(quote, newPrompt);
 
 
 
-function quote() {
-    alert('Hello World.....')
-};
+// function quote() {
+//     alert('Hello World.....')
+// };
 
-function endQuote(timer) {
-    clearTimeout(timer);
-    console.log(quote, newPrompt);
+// function endQuote() {
+//     clearTimeout(timer);
+//     console.log(quote, newPrompt);
 
-}
+// }
 
 
 
@@ -62,17 +62,6 @@ function endQuote(timer) {
 
 
 
-
-// const setPrompt = prompt("How often do you want to be alerted?")
-// const interval = setInterval(alerted, setPrompt);
-// function alerted() {
-//     alert(Math.random().toString(36).substring(7));
-//     console.log(alerted);
-// };
-
-// function endAlerted() {
-//     clearInterval(alerted);
-// };
 
 // // Immediately Invoked Function Expressions (IFFE)
 // (function() {
