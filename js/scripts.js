d0c14229670ebd1d71f839e73f3a45879c2f97b4
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2018-12-04 07:19:53 -0800

    setTimeout and setInterval

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,78 @@
-// Timers and set intervals
-// const newPrompt = prompt("How much time would you like to wait (in milliseconds)?");
-// console.log(newPrompt);
+// // // Timers and set intervals
+
+const newPrompt = prompt("How much time would you like to wait (in milliseconds)?");
+console.log(newPrompt);
+const timer = setTimeout(quote, newPrompt);
 
 
-    const newPrompt = prompt("How much time would you like to wait (in milliseconds)?");
-
-    console.log(newPrompt);
-    // const prompt = prompt("How much time would you like to wait (in milliseconds)?");
-    const timer = setTimeout(quote, newPrompt);
-    console.log(timer);
 
 function quote() {
-    alert('Hello World......')
+    alert('Hello World.....')
 };
+
+function endQuote(timer) {
+    clearTimeout(timer);
+    console.log(quote, newPrompt);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Set Interval and Stop Interval Methods
+// Example:
+
+// const setTime = setInterval(myTimer, 1000);
+
+// function myTimer() {
+//     const date = new Date();
+//     const time = date.toLocaleTimeString();
+//     document.getElementById("clock").innerHTML = time;
+// }
+
+// function myStopFunction() {
+//     clearInterval(setTime);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const setPrompt = prompt("How often do you want to be alerted?")
+// const interval = setInterval(alerted, setPrompt);
+// function alerted() {
+//     alert(Math.random().toString(36).substring(7));
+//     console.log(alerted);
+// };
+
+// function endAlerted() {
+//     clearInterval(alerted);
+// };
 
 // // Immediately Invoked Function Expressions (IFFE)
 // (function() {
